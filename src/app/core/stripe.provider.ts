1bed7c28fbcf981324c0aede01c15019b698a18b
Corey Sery <coreysery@gmail.com>
2019-03-10 12:30:28 -0700

    Stripe provider

--- src/app/core/stripe.provider.ts
+++ src/app/core/stripe.provider.ts
@@ -1,11 +1,13 @@
-import { InjectionToken } from '@angular/core';
+import { FactoryProvider, InjectionToken } from '@angular/core';
 import { stripePubKey } from 'app/globals';
 
 export const StripeToken = new InjectionToken<stripe.Stripe>('stripe');
 
 const Stripe = (<any>window).Stripe;
 
-export const StripeProvider = {
+export const getStripe = () => Stripe(stripePubKey);
+
+export const StripeProvider: FactoryProvider = {
   provide: StripeToken,
-  useFactory: () => Stripe(stripePubKey),
+  useFactory: getStripe,
 };
